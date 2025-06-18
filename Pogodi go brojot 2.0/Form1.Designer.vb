58f521e481ecafc2d457a937d3495159df5b5b1c
Oreskov Stojance <133867596+oreskovs@users.noreply.github.com>
2025-06-18 12:22:41 +0200

    Add files via upload

---
+++ Pogodi go brojot 2.0/Form1.Designer.vb
@@ -0,0 +1,156 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 200
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(68, 113)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(120, 186)
+        Me.ListBox1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.25!, System.Drawing.FontStyle.Bold)
+        Me.Label1.Location = New System.Drawing.Point(287, 36)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(194, 25)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Погоди го бројот"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.25!)
+        Me.Label2.Location = New System.Drawing.Point(227, 72)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(321, 17)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Замислив број од 0 до 99.Пробај да го погодиш"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 30.25!)
+        Me.TextBox1.Location = New System.Drawing.Point(309, 113)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(132, 118)
+        Me.TextBox1.TabIndex = 3
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(309, 248)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(132, 32)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Погоди"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(477, 150)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(90, 44)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "Нов број"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.25!)
+        Me.Label3.Location = New System.Drawing.Point(65, 326)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(0, 17)
+        Me.Label3.TabIndex = 6
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.25!)
+        Me.Label4.Location = New System.Drawing.Point(430, 326)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(0, 17)
+        Me.Label4.TabIndex = 7
+        '
+        'TextBox2
+        '
+        Me.TextBox2.BackColor = System.Drawing.Color.Red
+        Me.TextBox2.Location = New System.Drawing.Point(68, 87)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(120, 20)
+        Me.TextBox2.TabIndex = 8
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.CadetBlue
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox2 As TextBox
+End Class
---
+++ Pogodi go brojot 2.0/Form1.vb
@@ -0,0 +1,66 @@
+﻿Public Class Form1
+
+    Dim broj As Integer
+    Dim generator As New Random
+    Dim br_obidi As Integer
+    Dim vreme As Integer
+
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        broj = generator.Next(100)
+        br_obidi = 0
+        Timer1.Start()
+        vreme = Timer1.Interval
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        br_obidi = br_obidi + 1
+        Label3.Text = "Број на обиди " & br_obidi
+        ListBox1.Items.Add(TextBox1.Text)
+        If TextBox1.Text = broj Then
+            Label4.Text = "Браво погоди!"
+            Label4.Font = New Font("Courier Bold", 15)
+            Label4.ForeColor = Color.IndianRed
+            TextBox1.Enabled = False
+            Button1.Enabled = False
+            Timer1.Stop()
+        ElseIf TextBox1.Text > broj Then
+            Label4.Text = "Замислениот број е помал"
+        Else
+            Label4.Text = "Замислениот број е поголем"
+        End If
+        TextBox1.Text = ""
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        broj = generator.Next(100)
+        br_obidi = 0
+        TextBox1.Enabled = True
+        TextBox1.Text = " "
+        Label3.Text = " "
+        Label4.Text = " "
+        ListBox1.Items.Clear()
+        Label4.Font = New Font("Courier", 10)
+        Label4.ForeColor = Color.Black
+        Button1.Enabled = True
+        Timer1.Start()
+        vreme = Timer1.Interval
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        vreme = vreme - 1
+        If vreme = 0 Then
+            Timer1.Stop()
+            Label4.Text = "Времето истече! Обиди се повторно!"
+            Label4.Font = New Font("Courier Bold", 15)
+            Label4.ForeColor = Color.IndianRed
+            Button1.Enabled = False
+            TextBox1.Enabled = False
+        Else
+            TextBox2.Text = vreme & " сек"
+        End If
+    End Sub
+
+
+End Class
