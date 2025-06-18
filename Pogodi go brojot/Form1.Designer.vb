b499e40e9b7935455791c397f83a207b35cc18e6
Oreskov Stojance <133867596+oreskovs@users.noreply.github.com>
2025-06-18 12:22:05 +0200

    Add files via upload

---
+++ Pogodi go brojot/Form1.Designer.vb
@@ -0,0 +1,128 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.25!, System.Drawing.FontStyle.Bold)
+        Me.Label1.Location = New System.Drawing.Point(250, 33)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(242, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "ПОГОДИ ГО БРОЈОТ"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.25!)
+        Me.Label2.Location = New System.Drawing.Point(215, 76)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(321, 17)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Замислив број од 0 до 99.Пробај да го погодиш"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 30.25!)
+        Me.TextBox1.Location = New System.Drawing.Point(312, 122)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(116, 97)
+        Me.TextBox1.TabIndex = 2
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        Me.TextBox1.UseWaitCursor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(312, 234)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(116, 31)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Провери"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(504, 154)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(80, 33)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "Нов број"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.25!)
+        Me.Label3.Location = New System.Drawing.Point(87, 340)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(0, 16)
+        Me.Label3.TabIndex = 5
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.25!)
+        Me.Label4.Location = New System.Drawing.Point(545, 340)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(0, 16)
+        Me.Label4.TabIndex = 6
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Green
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Погоди го бројот"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+End Class
---
+++ Pogodi go brojot/Form1.vb
@@ -0,0 +1,40 @@
+﻿Public Class Form1
+
+    Dim generator As New Random
+    Dim broj As Integer
+    Dim br_obidi As Integer
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        broj = generator.Next(100)
+        br_obidi = 0
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        br_obidi = br_obidi + 1
+        Label3.Text = "Број на обиди " & br_obidi
+        If TextBox1.Text = broj Then
+            Label4.ForeColor = Color.IndianRed
+            Label4.Text = "Браво погоди!"
+            Label4.Font = New Font("Courier Bold", 14)
+            Button1.Enabled = False
+            TextBox1.Enabled = False
+        ElseIf TextBox1.Text > broj Then
+            Label4.Text = "Замислениот број е помал"
+        Else
+            Label4.Text = "Замислениот број е поголем"
+        End If
+        TextBox1.Text = ""
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        broj = generator.Next(100)
+        br_obidi = 0
+        Label4.ForeColor = Color.Black
+        Label4.Font = New Font("Courier", 9)
+        Button1.Enabled = True
+        TextBox1.Enabled = True
+        Label3.Text = ""
+        Label4.Text = ""
+        TextBox1.Text = ""
+    End Sub
+End Class
